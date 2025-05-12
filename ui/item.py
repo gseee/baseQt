65b01301a4397a3adad58dc35592a39741df5428
gseee <antoine_mallaroni@yahoo.fr>
2025-05-12 19:48:22 +0200

    [ADD] docstring

--- ui/item.py
+++ ui/item.py
@@ -1,4 +1,4 @@
-"""Items to use model and view."""
+"""Items to use on model and view."""
 from __future__ import annotations
 
 from collections.abc import Iterator
@@ -25,18 +25,25 @@ class TreeItem(Generic[TT]):
         self.__children = []
 
     def insert_child(self, children: TT, pos: int = -1):
-
+        """Insert given tree item in children in given position."""
         self.__children.insert(pos, children)
         children.parent = self
 
     def remove_child(self, child: TT):
+        """Remove given item from children."""
         self.__children.remove(child)
         child.parent = None
 
     def child(self, pos: int) -> TT:
+        """Return the child on given position."""
         return self.__children[pos]
 
     def iter_children(self, recursive: bool = False) -> Iterator[TT]:
+        """Iterate children.
+
+        Args:
+            recursive: Iterate recursively.
+        """
         for child in self.__children:
             yield child
 
@@ -44,6 +51,7 @@ class TreeItem(Generic[TT]):
                 yield from child.iter_children(recursive)
 
     def iter_parent(self) -> Iterator[TT]:
+        """Iterate all parents."""
         item = self
 
         while True:
@@ -58,22 +66,27 @@ class TreeItem(Generic[TT]):
 
     @property
     def child_count(self) -> int:
+        """Return the child count of the tree item."""
         return len(self.__children)
 
     @property
     def index(self) -> int:
+        """Return the item position of the item parent."""
         return self.__parent.children.index(self) if self.__parent else 0
 
     @property
     def children(self) -> list[TT]:
+        """Return children."""
         return self.__children
 
     @property
     def parent(self) -> TT | None:
+        """Return the parent of the tree item."""
         return self.__parent
 
     @parent.setter
     def parent(self, parent: TT | None):
+        """Set the parent of the item."""
         if self.__parent is not None:
             self.__parent.remove_child(self)
 
